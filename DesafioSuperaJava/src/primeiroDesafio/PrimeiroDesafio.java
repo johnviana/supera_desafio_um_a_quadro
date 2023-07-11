8b8a109adfbe848431e876ac9f698f9c2abb5b3f
wedsysa <johnvianaluz@hotmail.com>
2023-07-11 11:27:11 -0300

    codigo criado para numero pares em ordem crescente e impares para decrescente

--- DesafioSuperaJava/src/primeiroDesafio/PrimeiroDesafio.java
+++ DesafioSuperaJava/src/primeiroDesafio/PrimeiroDesafio.java
@@ -1,4 +1,44 @@
 package primeiroDesafio;
 
+import java.util.*;
+
+/**
+ * Dev: @john
+ */
 public class PrimeiroDesafio {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite a quantidade de número: ");
+        int numero = scanner.nextInt();
+
+        List<Integer> pares = new ArrayList<>();
+        List<Integer> impares = new ArrayList<>();
+
+        for(int i = 0; i < numero; i++){
+            System.out.print(  + i + 1 + ":" + " Número = ");
+            int valor = scanner.nextInt();
+            if(valor % 2 == 0){
+                pares.add(valor);
+            } else {
+                impares.add(valor);
+            }
+        }
+
+        Collections.sort(pares);
+
+        Collections.sort(impares, Comparator.reverseOrder());
+
+        for(int valor : pares ){
+            System.out.println("Números pares em ordem crecente " + valor);
+        }
+
+        System.out.println("====================================================");
+
+        for(int valor : impares){
+            System.out.println("Números impares em ordem decrescente " + valor);
+        }
+        scanner.close();
+    }
 }
